4ccc5582d12a99c8e3c2c7a2e9fcc3a9bbb95331
Alexey <apple230379@gmail.com>
2021-10-19 17:12:33 +0300

    Connected module popup.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,1 @@
-import {createSimilarObject} from './data';
-
-// Формирование массива из 10 сгенерированных элементов - новых объектов (объявлений об аренде)
-const SIMILAR_OBJECT_COUNT = 10;
-const similarObject = Array.from({length: SIMILAR_OBJECT_COUNT}, createSimilarObject);
-
-similarObject === similarObject; // Временная запись для ESLint
+import './popup.js';
