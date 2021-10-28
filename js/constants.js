9ffa045d6d8479e6e03615e302bd91f906f0d2d1
Alexey <apple230379@gmail.com>
2021-10-28 18:43:50 +0300

    Added input validation for form elements: title, price, rooms and guests

--- js/constants.js
+++ js/constants.js
@@ -1,6 +1,22 @@
 // Количество генерируемых объектов
 const SIMILAR_OBJECT_COUNT = 1;
 
+// Предельные значения кол-ва символов поля описания объявления
+const MIN_TITLE_LENGTH = 30;
+const MAX_TITLE_LENGTH = 100;
+
+// Предельные значения цены за ночь
+const MIN_PRICE_VALUE = 0;
+const MAX_PRICE_VALUE = 1000000;
+
+// Текст пользовательских сообщений валидации ввода текста и чисел
+const MIN_STRING_TEXT = 'Добавьте ещё символов';
+const MAX_STRING_TEXT = 'Удалите лишние символы';
+const MIN_NUMBER_TEXT = 'Число должно быть больше 0';
+const MAX_NUMBER_TEXT = 'Число должно быть меньше 1 000 000';
+const ROOM_CAPACITY_TEXT = 'Измените кол-во гостей или комнат';
+const MANDATORY_TEXT = 'Обязательное поле';
+
 // Массив сопоставления типов объекта
 const objectType = {
   flat: 'Квартира',
@@ -10,4 +26,4 @@ const objectType = {
   hotel: 'Отель',
 };
 
-export {objectType, SIMILAR_OBJECT_COUNT};
+export {objectType, SIMILAR_OBJECT_COUNT, MIN_TITLE_LENGTH, MAX_TITLE_LENGTH, MIN_PRICE_VALUE, MAX_PRICE_VALUE, MIN_STRING_TEXT, MAX_STRING_TEXT, MIN_NUMBER_TEXT, MAX_NUMBER_TEXT, ROOM_CAPACITY_TEXT, MANDATORY_TEXT};
